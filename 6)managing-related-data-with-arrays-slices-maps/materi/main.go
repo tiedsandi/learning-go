592ea96602a61fbd8827c58b5d09a6cfc5d3137b
tiedsandi <fachransandi@gmail.com>
2025-05-25 09:03:54 +0700

    6- make

--- 6)managing-related-data-with-arrays-slices-maps/materi/main.go
+++ 6)managing-related-data-with-arrays-slices-maps/materi/main.go
@@ -1,1 +1,36 @@
 package main
+
+import "fmt"
+
+// alias
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
+func main() {
+	userNames := make([]string, 2, 5) // type, default, max array
+	// userNames := []string{}
+
+	userNames[0] = "Joko"
+
+	userNames = append(userNames, "Fachran")
+	userNames = append(userNames, "sandi")
+
+	fmt.Println(userNames)
+
+	// new section
+
+	// courseRating := make(map[string]float64, 3)
+	courseRating := make(floatMap, 3)
+
+	courseRating["go"] = 4.7
+	courseRating["react"] = 4.9
+	courseRating["node"] = 4.9
+	courseRating["laravel"] = 4.9
+
+	courseRating.output()
+	// fmt.Println(courseRating)
+
+}
