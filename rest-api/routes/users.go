09991bc2ba55388fb2a2deb506e35d340104b3c4
tiedsandi <fachransandi@gmail.com>
2025-06-03 08:22:15 +0700

    ra: create user , signup routes

---
+++ rest-api/routes/users.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"net/http"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save data."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "User created successfully."})
+}
